90caff145e8c3efb5ae0aeacde5f867d390a113d
Durgeshyadav-91 <yadavdurgesh8160@gmail.com>
2023-12-30 18:57:35 +0530

    Complete update user functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -15,22 +15,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KET,
-//   authDomain: "mern-estate-c0603.firebaseapp.com",
-//   projectId: "mern-estate-c0603",
-//   storageBucket: "mern-estate-c0603.appspot.com",
-//   messagingSenderId: "516170972484",
-//   appId: "1:327166046831:web:06a7328ec9e86e720f939a"
-// };
-
-// // Initialize Firebase
-// export const app = initializeApp(firebaseConfig);
